db781364fda887bfb2b4a354f07d969ca58efee7
BrunoSaito <bruno.saito@usp.br>
2016-04-28 19:26:09 -0300

    getNext nao imbecil

--- EventoSet.java
+++ EventoSet.java
@@ -4,26 +4,19 @@ public class EventoSet {
 	private int index = 0;
 	private int next = 0;
 	public void add(Evento e) {
-		if(index >= events.length)
-			return;
 		events[index++] = e;
 	}
 	public Evento getNext() {
-		boolean looped = false;
-		int start = next;
-		do {
-			next = (next + 1) % events.length;
-			if(start == next) looped = true;
-			if((next == (start + 1) % events.length) && looped)
-				return null;
-		} while(events[next] == null);
-		return events[next];
+		int n = 0;
+		n = next;
+		next++;
+		return events[n];
 	}
 	public Evento viewNext() {
-		return events[next + 1];
+		return events[next];
 	}
 	public void removeCurrent() {
-		events[next] = null;
+		events[next-1] = null;
 	}
 	public void destroyEventoByDaniel() {
 		next++;
